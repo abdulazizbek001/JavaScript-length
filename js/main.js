b6590e80224fc4ff9fed5d769825526c4c95d340
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2023-12-02 16:53:55 +0500

    finish

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let user = prompt('Ismingizni kiriting');
+
+// let user2 = user.length;
+let user3 = user.length;
+
+// console.log(user2 + user3);
+
+const elTitle = document.querySelector('.title');
+
+elTitle.textContent =  user3
+
+// console.log(elTitle.textContent);
